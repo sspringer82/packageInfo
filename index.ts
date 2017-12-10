bf8073ba8a8873715ede591166a14d29025f842e
Sebastian Springer <info@sebastian-springer.com>
2017-12-10 16:53:18 +0100

    moved npm download stats to a new folder

--- index.ts
+++ index.ts
@@ -1,27 +1,4 @@
-import * as request from 'request';
-import { promisify } from 'util';
-
-const promisedRequest = <any>promisify(request);
-
-async function getDownloadStats(pkg: string) {
-  const [lastDay, lastWeek, lastMonth] = await Promise.all([
-    promisedRequest(getDownloadUrl(pkg, 'last-day')),
-    promisedRequest(getDownloadUrl(pkg, 'last-week')),
-    promisedRequest(getDownloadUrl(pkg, 'last-month')),
-  ]);
-  return {
-    lastDay: lastDay.body,
-    lastWeek: lastWeek.body,
-    lastMonth: lastMonth.body,
-  };
-}
-
-function getDownloadUrl(pkg: string, period: string) {
-  if (!['last-day', 'last-week', 'last-month'].includes(period)) {
-    throw new Error(`Period ${period} is not allowed`);
-  }
-  return `https://api.npmjs.org/downloads/point/${period}/${pkg}`;
-}
+import { getDownloadStats } from './npm/download-stats';
 
 (async () => {
   console.log(await getDownloadStats('gulp'));
---
+++ npm/download-stats.ts
@@ -0,0 +1,24 @@
+import * as request from 'request';
+import { promisify } from 'util';
+
+const promisedRequest = <any>promisify(request);
+
+function getDownloadUrl(pkg: string, period: string) {
+  if (!['last-day', 'last-week', 'last-month'].includes(period)) {
+    throw new Error(`Period ${period} is not allowed`);
+  }
+  return `https://api.npmjs.org/downloads/point/${period}/${pkg}`;
+}
+
+export async function getDownloadStats(pkg: string) {
+  const [lastDay, lastWeek, lastMonth] = await Promise.all([
+    promisedRequest(getDownloadUrl(pkg, 'last-day')),
+    promisedRequest(getDownloadUrl(pkg, 'last-week')),
+    promisedRequest(getDownloadUrl(pkg, 'last-month')),
+  ]);
+  return {
+    lastDay: lastDay.body,
+    lastWeek: lastWeek.body,
+    lastMonth: lastMonth.body,
+  };
+}
